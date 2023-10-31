90a53acfc924a6da808baaeff06e88c65704ef3b
Mircea <mirceaanghel01@gmail.com>
2023-10-31 11:29:02 +0200

    implemented class definition

--- src/Domain/People/Client.java
+++ src/Domain/People/Client.java
@@ -1,5 +1,18 @@
 package Domain.People;
 
-public class Client {
+public class Client extends Person {
+    private boolean membership;
+    private double amount_spent_at_hotel;
 
+    public Client() {}
+
+    public boolean isMembership() {return membership;}
+
+    public void setMembership(boolean membership) {this.membership = membership;}
+
+    public double getAmount_spent_at_hotel() {return amount_spent_at_hotel;}
+
+    public void setAmount_spent_at_hotel(double amount_spent_at_hotel) {
+        this.amount_spent_at_hotel = amount_spent_at_hotel;
+    }
 }
