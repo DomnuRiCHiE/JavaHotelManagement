ad9e81aced5a9fd50af5e88f6a70c129d83a5f6b
Mircea <mirceaanghel01@gmail.com>
2023-10-31 11:37:18 +0200

    getter and setter override for client from person

--- src/Domain/People/Client.java
+++ src/Domain/People/Client.java
@@ -6,6 +6,30 @@ public class Client extends Person {
 
     public Client() {}
 
+    @Override
+    public String getName() {return super.getName();}
+
+    @Override
+    public void setName(String name) {super.setName(name);}
+
+    @Override
+    public String getEmail() {return super.getEmail();}
+
+    @Override
+    public void setEmail(String email) {super.setEmail(email);}
+
+    @Override
+    public String getPhone_number() {return super.getPhone_number();}
+
+    @Override
+    public void setPhone_number(String phone_number) {super.setPhone_number(phone_number);}
+
+    @Override
+    public Address getAddress() {return super.getAddress();}
+
+    @Override
+    public void setAddress(Address address) {super.setAddress(address);}
+
     public boolean isMembership() {return membership;}
 
     public void setMembership(boolean membership) {this.membership = membership;}
