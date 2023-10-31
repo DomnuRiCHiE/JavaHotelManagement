7f49412d9454da95d464612f30ca4818d8f6dde1
Mircea <mirceaanghel01@gmail.com>
2023-10-31 22:13:00 +0200

    Start up screen for ui

--- src/UI/UI.java
+++ src/UI/UI.java
@@ -1,5 +1,7 @@
 package UI;
 
+import Controller.Controller;
+
 import javax.print.DocFlavor;
 import java.io.InputStream;
 import java.util.InputMismatchException;
@@ -8,43 +10,33 @@ import java.util.Scanner;
 public class UI {
 
     private String startUpMenu;
-    private String userContextMenu;
     private String adminContextMenu;
+
+    private Controller controller;
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
 
     public UI() {
         this.startUpMenu = "----------------StartUp------------------" +
-                "\n1.Login as User" +
-                "\n2.Login as Admin" +
-                "\n\n To login, please type user/admin";
+                "\n\n Press enter to log in";
+        this.adminContextMenu = "----------------Manage------------------" +
+                "\n1. Restaurant" +
+                "\n2. Bookings" +
+                "\n3. Rooms" +
+                "\n4. ";
     }
 
     public void run() throws InterruptedException {
         System.out.println(startUpMenu);
         Scanner loginOption = new Scanner(System.in);
         String option = loginOption.nextLine();
-        switch (option) {
-            case "user": {
-                System.out.println("Logging in...");
-                loginAsUser();
-                break;
-            }
-            case "admin": {
-                System.out.println("Logging in...");
-                loginAsAdmin();
-                break;
-            }
-            default: {
-                System.out.println("Wrong input! Try again.");
-                Thread.sleep(5);
-            }
-        }
+        System.out.println("Logging in...");
+        login();
     }
 
-    public void loginAsUser() {
-
-    }
-
-    public void loginAsAdmin() {
+    public void login() {
 
     }
 }
