fcee026f6b368539333d1244fa779c1ff427a02f
Mircea <mirceaanghel01@gmail.com>
2023-10-31 18:45:00 +0200

    Bookings class implementation

--- src/Domain/Hotel/Bookings.java
+++ src/Domain/Hotel/Bookings.java
@@ -1,5 +1,47 @@
 package Domain.Hotel;
 
-public class Bookings {
+import Domain.PaymentMethod;
+import Domain.People.Client;
 
+import java.util.ArrayList;
+
+public class Bookings {
+    private String ID;
+    private Client client;
+    private ArrayList<Room> bookedRooms;
+    private PaymentMethod paymentMethod;
+
+    public Bookings() {}
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public ArrayList<Room> getBookedRooms() {
+        return bookedRooms;
+    }
+
+    public void setBookedRooms(ArrayList<Room> bookedRooms) {
+        this.bookedRooms = bookedRooms;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
 }
