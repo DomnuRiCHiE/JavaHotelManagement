5d734e092396edb89fb646246c1f96a3cfcf26ce
Mircea <mirceaanghel01@gmail.com>
2023-10-31 21:58:47 +0200

    Start up screen for ui

--- src/UI/UI.java
+++ src/UI/UI.java
@@ -1,5 +1,50 @@
 package UI;
 
+import javax.print.DocFlavor;
+import java.io.InputStream;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class UI {
 
+    private String startUpMenu;
+    private String userContextMenu;
+    private String adminContextMenu;
+
+    public UI() {
+        this.startUpMenu = "----------------StartUp------------------" +
+                "\n1.Login as User" +
+                "\n2.Login as Admin" +
+                "\n\n To login, please type user/admin";
+    }
+
+    public void run() throws InterruptedException {
+        System.out.println(startUpMenu);
+        Scanner loginOption = new Scanner(System.in);
+        String option = loginOption.nextLine();
+        switch (option) {
+            case "user": {
+                System.out.println("Logging in...");
+                loginAsUser();
+                break;
+            }
+            case "admin": {
+                System.out.println("Logging in...");
+                loginAsAdmin();
+                break;
+            }
+            default: {
+                System.out.println("Wrong input! Try again.");
+                Thread.sleep(5);
+            }
+        }
+    }
+
+    public void loginAsUser() {
+
+    }
+
+    public void loginAsAdmin() {
+
+    }
 }
