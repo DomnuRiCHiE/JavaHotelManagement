687824fa3506c12a06a4cc34f5af909e806cab2f
Mircea <mirceaanghel01@gmail.com>
2023-11-11 10:27:31 +0200

    deleted implementation of interface ClientPerson

--- src/Factory/PersonFactory.java
+++ src/Factory/PersonFactory.java
@@ -3,9 +3,6 @@ package Factory;
 import Domain.People.Address;
 import Domain.People.Person;
 
-public abstract class PersonFactory implements CreatePerson{
-
+public abstract class PersonFactory{
     public abstract Person createPerson(String name, String email, String phone_number, Address address);
-
-
 }
