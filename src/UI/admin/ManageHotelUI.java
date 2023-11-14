ebe3fc7d5d05fd3bbe70cfadf8af7a11cfe4a5a3
Mircea <mirceaanghel01@gmail.com>
2023-11-14 19:36:13 +0200

    changed

--- src/UI/admin/ManageHotelUI.java
+++ src/UI/admin/ManageHotelUI.java
@@ -5,17 +5,31 @@ import Domain.Restaurant.Restaurant;
 
 public interface ManageHotelUI {
     public void assignRestaurantToHotel();
-    public void manageRestaurant();
+
+    public void manageRestaurant(); // uses ManageRestaurantUI
+
+    public void manageRoom(); // uses ManageRoomUI
+
     public void getClientsInfoList();
+
     public void searchClientByName();
+
     public void addRoomToHotel();
+
     public void deleteRoomFromHotel();
+
     public void deleteClientFromHotel();
+
     public void modifyClientName();
+
     public void modifyClientPhoneNumber();
+
     public void modifyClientAddress();
+
     public void modifyClientEmail();
+
     public void modifyBookingInfo();
+
     public void searchBookingByClientName();
 
 }
