9db67864bc367b4ad4e1c3917903bd156f544807
Mircea <mirceaanghel01@gmail.com>
2023-11-14 18:29:40 +0200

    changed definition

---
+++ src/UI/AdminUI.java
@@ -0,0 +1,7 @@
+package UI;
+
+public interface AdminUI extends UInterface{
+    public void manageRooms();
+    public void manageHotel();
+    public void manageRestaurantMenu();
+}
---
+++ src/UI/ClientUI.java
@@ -0,0 +1,7 @@
+package UI;
+
+public interface ClientUI extends UInterface{
+    public void showClientOptions();
+    public void bookHotel();
+    public void placeRestaurantOrder();
+}
