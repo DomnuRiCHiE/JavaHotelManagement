9da8ef1dc8d6e48badfb89fa2c4ce1457104b424
Mircea <mirceaanghel01@gmail.com>
2023-11-14 18:12:49 +0200

    defined UInterface

---
+++ src/UI/UInterface.java
@@ -0,0 +1,8 @@
+package UI;
+
+public interface UInterface {
+    public void run();
+    public void selectWelcomeScreenOptions(String option);
+    public void loginTypeUsernameScreen();
+    public void loginTypePasswordScreen();
+}
