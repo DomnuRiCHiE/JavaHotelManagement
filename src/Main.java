dcdc189c844a4ce9f95412e500cac5b897d71d72
Mircea <mirceaanghel01@gmail.com>
2023-11-14 18:12:29 +0200

    slight changes

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,6 @@ import UI.UI;
 public class Main {
     public static void main(String[] args) {
         UI ui = new UI();
-        try{
-            ui.run();
-
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        ui.run();
     }
 }
