ce6b070a8bf590beb95e96680d4a0a54e972c92d
Mircea <mirceaanghel01@gmail.com>
2023-11-14 19:18:34 +0200

    changed definition

---
+++ src/UI/admin/ManageRoomUI.java
@@ -0,0 +1,14 @@
+package UI.admin;
+
+import Domain.Hotel.Hotel;
+import Domain.Hotel.Room;
+import Domain.Hotel.RoomCategories;
+
+public interface ManageRoomUI {
+    public void changeRoomType();
+    public void getRoomInfo();
+    public void changeOccupancyStatus();
+    public void changePrice();
+
+
+}
