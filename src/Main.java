d208470d1cc06d412a8dc03107a6109ec94eba60
Mircea <mirceaanghel01@gmail.com>
2023-11-18 16:23:42 +0200

    changed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import Domain.People.Admin;
 import UI.UI;
 
 public class Main {
     public static void main(String[] args) {
+        Admin admin = Admin.getInstance();
+        admin.setPassword("password");
         UI ui = new UI();
         ui.run();
     }
