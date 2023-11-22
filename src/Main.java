406c349f03f7f5b03d49eb808f86d2d06ebb5669
Mircea <mirceaanghel01@gmail.com>
2023-11-22 08:29:30 +0200

    updated Main.java to use Factory Patter

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import Controller.People.ClientController;
 import Domain.People.Address;
 import Domain.People.Admin;
 import Domain.People.Client;
+import Factory.ClientFactory;
 import Repository.Hotel.BookingRepository;
 import Repository.Hotel.HotelRepository;
 import Repository.Hotel.RoomRepository;
@@ -16,10 +17,13 @@ public class Main {
         Admin admin = Admin.getInstance();
         admin.setPassword("password");
 
+        ClientFactory factory = new ClientFactory();
 
         ClientController clientController = new ClientController(ClientRepository.getInstance());
-        Client client1 = new Client("bob","bob@gmail.com","0700800900",new Address(),"bobzob");
-        Client client2 = new Client("zob","zob@gmail.com","0700810920",new Address(),"zobbob");
+        Client client1 = factory.createPerson("bob","bob@gmail.com","0700800900", new Address());
+        factory.createPassword(client1,"bobzob");
+        Client client2 = factory.createPerson("zob","zob@gmail.com","0700810920",new Address());
+        factory.createPassword(client2,"zobbob");
         clientController.add(client1);
         clientController.add(client2);
         BookingController bookingController = new BookingController(BookingRepository.getInstance());
