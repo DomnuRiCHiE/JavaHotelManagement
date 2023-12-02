f80ba2b4d45a5b1bce1e72cb352a753805fbdc7a
robertburlacu <robert.burlacu03@gmail.com>
2023-12-03 00:38:07 +0200

    Database connection - initial files - DAO interface - DAO connection class

---
+++ src/DAO/DatabaseConnection.java
@@ -0,0 +1,25 @@
+package DAO;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    private static Connection connection;
+
+    private DatabaseConnection() {}
+
+    public static void setConnection(String url, String user, String password) throws SQLException
+    {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (Exception exception) {
+            throw new RuntimeException();
+        }
+        connection = DriverManager.getConnection(url, user, password);
+    }
+
+    public static Connection getConnection()
+    {
+        if (connection == null) throw new RuntimeException("Database connection failed");
+        return connection;
+    }
+}
