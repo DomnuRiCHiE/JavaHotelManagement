cf666efd48bbe88446d818c3635e379ea6d92f2d
Mircea <mirceaanghel01@gmail.com>
2023-12-06 19:52:40 +0200

    updated to include AlcoholicBeverageDAO access

--- src/Controller/Restaurant/AlcoholicBeveragesController.java
+++ src/Controller/Restaurant/AlcoholicBeveragesController.java
@@ -3,9 +3,33 @@ package Controller.Restaurant;
 import Controller.MainController;
 import Domain.Restaurant.AlcoholicBeverages;
 import Repository.Restaurant.AlcoholicBeverageRepository;
+import DAO.AlcoholicBeverageDAO;
+
+import java.util.ArrayList;
 
 public class AlcoholicBeveragesController extends MainController<AlcoholicBeverages> {
-    public AlcoholicBeveragesController(AlcoholicBeverageRepository alcoholicBeverageRepository) {
-        super(alcoholicBeverageRepository);
+    public AlcoholicBeveragesController(AlcoholicBeverageRepository alcoholicBeverageRepository, AlcoholicBeverageDAO alcoholicBeverageDAO) {
+        super(alcoholicBeverageRepository, alcoholicBeverageDAO);
+    }
+
+    @Override
+    public void add(AlcoholicBeverages object) {
+        DAO_repo.addEntityDAO(object);
+    }
+
+    @Override
+    public void update(AlcoholicBeverages object1, AlcoholicBeverages object2) {
+        DAO_repo.updateEntityDAO(object2);
+    }
+
+    @Override
+    public void delete(AlcoholicBeverages object) {
+        DAO_repo.deleteEntityDAO(object);
+
+    }
+
+    @Override
+    public ArrayList<AlcoholicBeverages> getAll() {
+        return DAO_repo.getAllEntitiesDAO();
     }
 }
